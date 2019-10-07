2e6cb37c80e79d7327c1f22963b1a44290f315e3
reutskij daniil <reutskij.da@phystech.edu>
2019-10-08 00:43:16 +0300

    latest

---
+++ request.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(0);
+if (!isset($_GET["name"])) exit;
+$name = $_GET["name"];
+$data = json_decode(file_get_contents("https://rest.bandsintown.com/artists/$name/events?app_id=1"), 1);
+$result = array();
+foreach ($data as $item) {
+  $venue = $item["venue"];
+  $result[] = array(
+    "lat" => $venue["latitude"],
+    "lng" => $venue["longitude"],
+    "city" => $venue["city"],
+    "datetime" => $item["datetime"],
+    "url" => $item["url"]
+  );
+}
+echo json_encode($result);
+?> 
