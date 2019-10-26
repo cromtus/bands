231fae4de269e58d8fea57c94ec8e03046203fc1
Arkady Bazhanov <arkady.bazhanov@gmail.com>
2019-10-27 00:51:21 +0300

    Add support for special characters (like space) in search

--- suggestions.php
+++ suggestions.php
@@ -2,7 +2,8 @@
 error_reporting(0);
 if (!isset($_GET["term"])) exit;
 $searchTerm = $_GET["term"];
-$data = json_decode(file_get_contents("https://www.bandsintown.com/searchSuggestions?searchTerm=$searchTerm"), 1);
+
+$data = json_decode(file_get_contents("https://www.bandsintown.com/searchSuggestions?searchTerm=" . urlencode($searchTerm)), 1);
 $result = array();
 foreach ($data["artists"] as $item) {
   $result[] = array(
