6b60a2e235fd6d988fc228d3d4ec29f49bd1c579
nepoklonov <i.s.nepoklonov@yandex.ru>
2023-09-30 01:16:43 +0330

    API access restored

--- request.php
+++ request.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 if (!isset($_GET["name"])) exit;
 $name = $_GET["name"];
-$data = json_decode(file_get_contents("https://rest.bandsintown.com/artists/$name/events?app_id=1"), 1);
+$data = json_decode(file_get_contents("https://rest.bandsintown.com/artists/$name/events?app_id=0"), 1);
 $result = array();
 foreach ($data as $item) {
   $venue = $item["venue"];
